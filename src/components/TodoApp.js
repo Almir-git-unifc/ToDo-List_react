bd555500d241189f2a4a4976c4f91795bc59cf30
Almir-git-unifc <1341215@sga.pucminas.br>
2022-05-18 20:39:52 -0500

    feat(js): v01.02.000 - React JS and hook,  input field text appears in rectangles below this field

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useState } from "react"
 import "./todoapp.css";
 
-
 function TodoApp() {
-    return (   
-     <div className="todo">
-           <input type="text" name="text" id="text" placeholder="Add task here..." />
-           <button className="add-btn">Add</button>
-     </div>
-     );
-    }
-    
-    export default TodoApp;
+    const [task, setTask] = useState("");
+    const [tasklist, setTaskList] = useState([]);
+
+    const handleChange = (e) => {
+        setTask(e.target.value);
+    };
+
+    const AddTask = () => {
+        if (task !== "") {
+            const taskDetails = {
+                id: Math.floor(Math.random() * 1000),
+                value: task,
+                isCompleted: false,
+            };
+
+            setTaskList([...tasklist, taskDetails]);
+        }
+    };
+
+    console.log("tasklist", tasklist);
+
+
+    return (
+        <div className="todo">
+            <input
+                type="text"
+                name="text"
+                id="text"
+                onChange={(e) => handleChange(e)}
+                placeholder="Add task here..."
+            />
+            <button className="add-btn" onClick={AddTask}>
+                Add
+            </button>
+            <br />
+            {tasklist !== [] ? (
+                <ul>
+                    {tasklist.map((t) => (
+
+                        <li className="listitem">{t.value}</li>
+                    ))}
+                </ul>
+            ) : null}
+        </div>
+    );
+}
+
+export default TodoApp;
